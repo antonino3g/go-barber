ffcfcfd2ed80e8ab6fbdbe6e68a29dcbc339ca53
Antonino Praxedes <antonino.praxedes@gmail.com>
2020-09-23 00:04:03 -0400

    feat: adding list available hour

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import { getRepository, Repository, Raw } from 'typeorm';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -34,6 +35,28 @@ class AppointmentsRepository implements IAppointmentsRepository {
         date: Raw(
           dateFildName =>
             `to_char(${dateFildName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = String(day).padStart(2, '0');
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFildName =>
+            `to_char(${dateFildName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
