169abef2a15998345df0fe4e3aebca5c06c31cc2
Antonino Praxedes <antonino.praxedes@gmail.com>
2020-09-23 20:33:25 -0400

    feat: adding function delete old hours

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -34,7 +34,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFildName =>
-            `to_char(${dateFildName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+            `to_char(${dateFildName},'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
     });
@@ -56,7 +56,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFildName =>
-            `to_char(${dateFildName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+            `to_char(${dateFildName},'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
